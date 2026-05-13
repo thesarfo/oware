4e74eefe1079bf026bb046f71a74b7c809500a04
blackprince001 <appiahboaduprince@gmail.com>
2026-05-13 22:28:23 +0000

    logo and favicon

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,6 @@
 import { PlayPage } from "./pages/PlayPage";
+import { MatchPage } from "./pages/MatchPage";
+import { LeaderboardPage } from "./pages/LeaderboardPage";
 import { GamesPage } from "./pages/GamesPage";
 import { ReplayPage } from "./pages/ReplayPage";
 import { StatsPage } from "./pages/StatsPage";
@@ -6,8 +8,10 @@ import { useHashRoute } from "./hooks/useHashRoute";
 
 export function App() {
   const [route] = useHashRoute();
-  if (route.name === "games") return <GamesPage scope={route.scope} />;
-  if (route.name === "stats") return <StatsPage scope={route.scope} />;
+  if (route.name === "match") return <MatchPage />;
+  if (route.name === "leaderboard") return <LeaderboardPage />;
+  if (route.name === "games") return <GamesPage scope={route.scope} kind={route.kind} />;
+  if (route.name === "stats") return <StatsPage scope={route.scope} kind={route.kind} />;
   if (route.name === "replay")
     return <ReplayPage gameId={route.gameId} scope={route.scope} />;
   return <PlayPage />;
